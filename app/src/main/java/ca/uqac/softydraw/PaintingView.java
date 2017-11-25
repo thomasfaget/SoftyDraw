679a812be3f150348c4297a2ffe0047ac3e1ae1f
thomas <thomas.faget@etu.enseeiht.fr>
2017-11-25 01:19:23 -0500

    Update : Can draw, change current color and change available colors

---
+++ app/src/main/java/ca/uqac/softydraw/PaintingView.java
@@ -0,0 +1,112 @@
+package ca.uqac.softydraw;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+/** A View used to paint on the screen
+ */
+
+public class PaintingView extends View {
+
+    //drawing path
+    private Path drawPath;
+    //drawing and canvas paint
+    private Paint drawPaint, canvasPaint;
+    //canvas
+    private Canvas drawCanvas;
+    //canvas bitmap
+    private Bitmap canvasBitmap;
+
+
+    public PaintingView(Context context) {
+        super(context);
+    }
+
+    public PaintingView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PaintingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public PaintingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    /** Initialize data of the Painting view
+     */
+    public void initialDrawing() {
+        drawPath = new Path();
+        drawPaint = new Paint();
+
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    /** Change the current color
+     */
+    public void setCurrentColor(int color) {
+        invalidate();
+        drawPaint.setColor(color);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        Log.d("TEST", "coucou");
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawPath.lineTo(touchX, touchY);
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+
+        invalidate();
+        return true;
+    }
+
+}
--- app/src/main/java/ca/uqac/softydraw/SoftyDrawActivity.java
+++ app/src/main/java/ca/uqac/softydraw/SoftyDrawActivity.java
@@ -7,8 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ca.uqac.softydraw.colorPicker.ColorPickerDialog;
 
@@ -16,9 +18,9 @@ import ca.uqac.softydraw.colorPicker.ColorPickerDialog;
 public class SoftyDrawActivity extends AppCompatActivity {
 
     // Views
-    private PaintView paintView;
-    private ScreenListener screenListener;
-    private Button testButton;
+    private PaintingView paintingView;
+
+    private ImageButton currentButtonUsedToDraw;
 
 
     @Override
@@ -32,31 +34,74 @@ public class SoftyDrawActivity extends AppCompatActivity {
         setContentView(R.layout.activity_softy_draw);
 
         // View
-        paintView = findViewById(R.id.paintView);
-        testButton = findViewById(R.id.testButton);
+        paintingView = findViewById(R.id.paintView);
+        List<ImageButton> colorButtons= new ArrayList<>();
+        colorButtons.add((ImageButton) findViewById(R.id.colorButton1));
+        colorButtons.add((ImageButton) findViewById(R.id.colorButton2));
+        colorButtons.add((ImageButton) findViewById(R.id.colorButton3));
+        colorButtons.add((ImageButton) findViewById(R.id.colorButton4));
+        colorButtons.add((ImageButton) findViewById(R.id.colorButton5));
+        colorButtons.add((ImageButton) findViewById(R.id.colorButton6));
 
-        testButton.setOnClickListener(new View.OnClickListener() {
+        View.OnLongClickListener changeColorListener = new View.OnLongClickListener() {
             @Override
-            public void onClick(View view) {
-                showColorPicker();
+            public boolean onLongClick(View view) {
+                ImageButton button = (ImageButton) view;
+                changeColorButton(button);
+                return true;
             }
-        });
-        screenListener = new ScreenListener(paintView);
-        paintView.setOnTouchListener(screenListener);
+        };
 
+        // Set up color button listener
+        for (ImageButton button: colorButtons) {
+            button.setOnLongClickListener(changeColorListener);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ImageButton button = (ImageButton) view;
+                    changeCurrentColor(button);
+                }
+            });
+        }
+
+        // Setup paintingView
+        paintingView.initialDrawing();
+        changeCurrentColor(colorButtons.get(0)); // Set first color
     }
 
-    /** Display the color picker in order to change a color
+    /** Display the color picker in order to change the color of a button
+     *
+     * @param button the button which to change the color
      */
-    private void showColorPicker() {
+    private void changeColorButton(final ImageButton button) {
         int initialColor = Color.WHITE;
         ColorPickerDialog colorPickerDialog = new ColorPickerDialog(this, initialColor, new ColorPickerDialog.OnColorSelectedListener() {
             @Override
             public void onColorSelected(int color) {
-                testButton.setBackgroundColor(color);
+                // Change the color of the button
+                button.setTag(String.format("#%06X", 0xFFFFFF & color));
+                button.setBackgroundColor(color);
+                // Chane current color
+                changeCurrentColor(button);
             }
         });
         colorPickerDialog.show();
+    }
+
+    /** Change the current color in the interface and in the DrawingView
+     */
+    private void changeCurrentColor(ImageButton button) {
+
+        // Change color used to draw
+        int color = Color.parseColor((String) button.getTag());
+        paintingView.setCurrentColor(color);
+        // Change current button used to draw in the interface
+        button.setImageDrawable(getResources().getDrawable(R.drawable.button_select_color_pressed));
+        if (currentButtonUsedToDraw != null && currentButtonUsedToDraw != button) {
+            currentButtonUsedToDraw.setImageDrawable(getResources().getDrawable(R.drawable.button_select_color));
+        }
+        currentButtonUsedToDraw = button;
+
     }
 
 
